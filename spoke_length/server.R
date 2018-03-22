4c3ccf632e02d62aee4351b52c7e066e09ae9fb7
jannes-m <malnamalja@gmx.de>
2018-03-22 12:33:05 +0100

    put deployment procedure into an Rmd-file

--- spoke_length/server.R
+++ spoke_length/server.R
@@ -77,20 +77,3 @@ shinyServer(
   }
 )
 
-# What to do next:
-# 2. helpful error messages:
-# http://shiny.rstudio.com/articles/validation.html
-
-# shinyapps::showLogs
-# To make the function actually work on the shiny server, I had to:
-# create locally a folder which should contain all necessary packages 
-# dir.create("D:/R/lib")
-# .libPaths("D:/R/lib")
-# install the velo package from github
-# devtools::install_github("jannes-m/velo")
-# devtools::install_github("daattali/shinyjs")
-# deploy the app
-# setwd("C:/Users/Jannes/Desktop/spoke_app")
-# https://jannes-m.shinyapps.io/spoke_app/
-# library(shinyapps)
-# deployApp()
